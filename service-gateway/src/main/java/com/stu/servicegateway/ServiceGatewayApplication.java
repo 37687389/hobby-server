945ec742bbbcd5455d59570c4131bc7946ae98b6
596842002@qq.com <qaz553>
2019-12-09 23:57:35 +0800

    集成eureka getway

---
+++ service-gateway/src/main/java/com/stu/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.stu.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceGatewayApplication.class, args );
+        System.out.println("ServiceGatewayApplication 启动成功");
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+}
---
+++ service-hei/src/test/java/com/stu/servicehei/ScFGetwayCloundApplicationTests.java
@@ -0,0 +1,11 @@
+package com.stu.servicehei;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScFGetwayCloundApplicationTests {
+
+    void contextLoads() {
+    }
+
+}

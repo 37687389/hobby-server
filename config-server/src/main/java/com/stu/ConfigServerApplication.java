77afe7d6793f0d9d919bc4d33f16846537b6d110
596842002@qq.com <qaz553>
2020-01-04 16:46:34 +0800

    添加配置中心

---
+++ config-server/src/main/java/com/stu/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("配置中心");
+	}
+}
